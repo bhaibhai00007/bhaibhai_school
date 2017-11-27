05902ceee5eb27984f07302a7cf1c619479dcc68
bhaibhai00007 <bhaibhai00007@gmail.com>
2017-11-27 23:33:03 +0530

    only push

--- application/controllers/principal/Ajax_controller_principal.php
+++ application/controllers/principal/Ajax_controller_principal.php
@@ -8,6 +8,7 @@ class Ajax_controller_principal extends MY_Controller {
         parent::__construct();
     }
 
+    ///teacher section starts
     function add_teacher() {
         $this->load->model('Sc_teacher_model');
         $this->load->model('Sc_user_model');
@@ -36,6 +37,7 @@ class Ajax_controller_principal extends MY_Controller {
             foreach ($tableTeacherStructureForeignKeyIdArr AS $key => $val) {
                 $teacherDataArr[$key] = $this->input->post($key, TRUE);
             }
+
             $DOBDate = DateTime::createFromFormat('d-m-Y', $teacherDataArr['DOB']);
             $teacherDataArr['DOB'] = $DOBDate->format('Y-m-d');
             $DOJDate = DateTime::createFromFormat('d-m-Y', $teacherDataArr['DOJ']);
@@ -263,7 +265,8 @@ class Ajax_controller_principal extends MY_Controller {
             }
         }
     }
-
+    ///teacher section ends here
+    ///parents section starts here
     function add_parent() {
         $this->load->model('Sc_parent_model');
         $this->load->model('Sc_user_model');
@@ -474,4 +477,177 @@ class Ajax_controller_principal extends MY_Controller {
             }*/
         }
     }
+
+    //parents section ends here
+    //holiday section starts here
+    function add_holiday() {
+        $this->load->model('Sc_holiday_model');
+        $tableHolidayStructureTextArr = $this->Sc_holiday_model->_table_holiday_structure_text;
+        $formValidationConfigArr = array();
+        $formValidationConfigArr = generate_form_validation_arr($tableHolidayStructureTextArr);
+        $this->form_validation->set_rules($formValidationConfigArr);
+        if ($this->form_validation->run() == FALSE) {
+            echo json_encode(array('result' => 'bad', 'msg' => str_replace('</p>', '', str_replace('<p>', '', validation_errors()))));die;
+        } else {
+            $holidayDataArr = array();
+            foreach ($tableHolidayStructureTextArr AS $key => $val) {
+                $holidayDataArr[$key] = $this->input->post($key, TRUE);
+            }
+            $startDate = DateTime::createFromFormat('d-m-Y', $holidayDataArr['startDate']);
+            $holidayDataArr['startDate'] = $startDate->format('Y-m-d');
+            $endDate = DateTime::createFromFormat('d-m-Y', $holidayDataArr['endDate']);
+            $holidayDataArr['endDate'] = $endDate->format('Y-m-d');
+            //pre($holidayDataArr); die;
+            $holidayId = $this->Sc_holiday_model->add($holidayDataArr);
+            if ($holidayId != "") {
+                echo json_encode(array('result' => 'good', 'msg' => 'holiday added successfully.'));die;
+            }else{
+                echo json_encode(array('result' => 'bad', 'msg' => 'Unknown Error.'));die; 
+            }
+        }
+    }
+
+    function show_holiday_list_in_update_data_table(){
+        $this->load->model('Sc_holiday_model');
+        $holidayDataArr=  $this->Sc_holiday_model->get_holiday_list_for_principal();
+        ob_start();
+         if (!empty($holidayDataArr)):
+            foreach ($holidayDataArr AS $key =>$value):?>
+        <tr>
+            <td class="center-align">
+                <input type="checkbox" id="holiday<?php echo $value['holidayId'];?>">
+                <label for="holiday<?php echo $value['holidayId'];?>"></label>
+            </td>
+            <td data-id="<?php echo $value['holidayId'];?>"></td>
+            <td><?php echo $value['title'];?></td>
+            <td><?php echo $value['startDate'];?></td>
+            <td><?php echo $value['endDate'];?></td>
+            <td class="center-align">
+                <div class="btn-group">
+                    <a href="javascript:void(0);" class="btn-flat btn-small waves-effect">
+                        <i class="material-icons">edit</i>
+                    </a>
+                    <a class="btn-flat btn-small waves-effect btnDelete">
+                        <i class="material-icons">delete</i>
+                    </a>
+                </div>
+            </td>
+        </tr>
+        <?php endforeach;
+        endif;
+        $contents = ob_get_contents();
+	ob_end_clean();
+        echo $contents;die;
+    }
+
+    function holiday_delete(){
+        $this->load->model('Sc_holiday_model');
+        $holidayId= $this->input->post('holidayId',TRUE);
+        /// do transaction check stuff here; if valid then start process for delete teacher
+        if($this->Sc_holiday_model->delete($holidayId)==TRUE){
+            echo json_encode(array('result' => 'good', 'msg' => 'holiday delete successfully.'));die;
+        }else{
+            echo json_encode(array('result' => 'bad', 'msg' => 'Unknown error arises for delete the holiday.'));die;
+        }
+    }
+
+    function get_holiday_details_with_edit_mode(){
+        $holidayId= $this->input->post("holidayId",TRUE);
+        if($holidayId==""){
+            echo json_encode(array('result' => 'bad', 'msg' => 'Inalid holiday index for update.'));die;
+        }else{
+            $this->load->model('Sc_holiday_model');
+            // $this->load->model('Sc_holiday_model');
+            $dataArr= $this->Sc_holiday_model->get_full_details_by_id($holidayId);
+            $table_holiday_structure_text= $this->Sc_holiday_model->_table_holiday_structure_text;
+            // $table_user_structure_text= $this->Sc_parent_model->_table_user_structure_text;
+            $table_user_structure_text_arr=array();
+            foreach($table_holiday_structure_text AS $k =>$v){
+                if(array_key_exists('not_editable', $v)){
+                    $valueProp=$dataArr[0][$k];
+                }else{
+                    $valueProp='value="'.$dataArr[0][$k].'"';
+                }
+                $v['elementEditVal']=$valueProp;
+                $table_user_structure_text_arr[$k]=$v;
+            }
+            
+            // $table_parent_structure_text_arr=array();
+            // foreach($table_parent_structure_text AS $k =>$v){
+            //     $valueProp='value="'.$dataArr[0][$k].'"';
+            //     $v['elementEditVal']=$valueProp;
+            //     $table_parent_structure_text_arr[$k]=$v;
+            // }
+           
+            $data=array();
+            $data['table_user_structure_text']=$table_holiday_structure_text_arr;
+            //$data['table_parent_structure_text']=$table_parent_structure_text_arr;
+            //pre($dataArr);die;
+            $data['holidayDataArr']=$dataArr[0];
+            $data['primary_key_field']= $this->Sc_holiday_model->_table_primary_key;
+            $data['primary_key_field_val']=$holidayId;
+            $data['common_css'] = $this->load->view('common_css', $data, true);
+            $data['common_js'] = $this->load->view('common_js', $data, true);
+            $viewContent= $this->load->view($this->erpUserTypeArr[$this->userType].'/modal_holiday_edit',$data,TRUE);
+            
+            echo json_encode(array('result' => 'good', 'resultContent' => $viewContent));die;
+        }
+    }
+
+    function holiday_status_chanage(){
+        $teacherId= $this->input->post('holidayId',TRUE);
+        $changeTo=$this->input->post('changeTo',TRUE);
+        
+        $this->load->model('Sc_holiday_model');
+        // $this->load->model('Sc_user_model');
+        
+        $HolidayDataArr= $this->Sc_holiday_model->get_details_by_id($holidayId);
+        if(empty($HolidayDataArr)){
+            echo json_encode(array('result' => 'bad', 'msg' => 'invalie holiday selected for status update.'));die;
+        }else{
+            if($this->Sc_user_model->edit(array('status'=>$changeTo),$HolidayDataArr['userId'])==TRUE){
+                echo json_encode(array('result' => 'good', 'msg' => 'holiday status change successfully.'));die;
+            }else{
+                echo json_encode(array('result' => 'bad', 'msg' => 'Unknown error arises for update the holiday status.'));die;
+            }
+        }
+    }
+
+    function edit_holiday(){
+        //pre($_POST);die;
+        $this->load->model('Sc_holiday_model');
+        // $this->load->model('Sc_user_model');
+        $tableHolidayStructureTextArr = $this->Sc_holiday_model->_table_holiday_structure_text;
+        // $tableUserStructureTextArr=$this->Sc_teacher_model->_table_user_structure_text;
+        
+        $formValidationConfigArr = array();
+        $formValidationConfigArr = generate_form_validation_arr($tableHolidayStructureTextArr,array(),TRUE);
+        // $formValidationConfigArr = generate_form_validation_arr($tableUserStructureTextArr, $formValidationConfigArr,TRUE);
+        
+        //pre($formValidationConfigArr);die;
+        
+        $this->form_validation->set_rules($formValidationConfigArr);
+        if ($this->form_validation->run() == FALSE) {
+            echo json_encode(array('result' => 'bad', 'msg' => str_replace('</p>', '', str_replace('<p>', '', validation_errors()))));die;
+        } else {
+            $primeryKeVal= $this->input->post($this->Sc_holiday_model->_table_primary_key,TRUE);
+            $oldHolidayDataArr= $this->Sc_holiday_model->get_details_by_id($primeryKeVal);
+            //pre($teacherDataArr);die;
+            // $userDataArr = array();
+            // $userDataArr = generate_user_table_data_arr_for_edit($tableUserStructureTextArr, array('typeText' => 'parent'));
+            // //pre($userDataArr);die;
+           
+            
+            if($this->Sc_holiday_model->edit($holidayDataArr,$primeryKeVal)){
+                //echo json_encode(array('result' => 'good', 'msg' => 'holiday updated successfully.'));die;
+                $this->session->set_flashdata('success_message','holiday updated successfully.');
+                redirect(BASE_URL.$this->erpUserTypeArr[$this->userType].'/principal/show_holiday_list');
+            }
+            /*if ($holidayId != "") {
+                echo json_encode(array('result' => 'good', 'msg' => 'holiday updated successfully.'));die;
+            }*/
+        }
+    }
+
+    ///holiday section ends here.
 }
